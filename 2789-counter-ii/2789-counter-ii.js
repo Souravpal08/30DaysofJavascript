32f20ad7ade5a60c6a20f6f1b1ecdea6333d409c
Sourav Pal <143326384+Souravpal08@users.noreply.github.com>
2025-03-27 19:14:50 +0530

    Time: 55 ms (43.58%), Space: 57.8 MB (11.68%) - LeetHub

---
+++ 2789-counter-ii/2789-counter-ii.js
@@ -0,0 +1,32 @@
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    let currentVal = init;
+    return {
+        increment: function(){
+            currentVal += 1;
+            return currentVal;
+        },
+        decrement : function(){
+            currentVal -= 1;
+            return currentVal;
+        },
+        reset : function(){
+            currentVal = init;
+            return currentVal
+        }
+    }
+};
+ const counter = createCounter (5)
+ console.log(counter.increment());
+ console.log(counter.reset());
+ console.log(counter.decrement());
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
